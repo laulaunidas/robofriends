70615b2222c5d1d6ae25b19c961e14e675166f1d
laulaunidas <laulaunidas@gmail.com>
2018-05-10 16:38:36 +0200

    react origin without build

---
+++ src/robots.js
@@ -0,0 +1,21 @@
+export const robots = [
+    {
+        id:1,
+        name:"leane Minerbe",
+        username:"doudou",
+        email:"leane@gmail.com"
+    },
+    {
+        id:2,
+        name:"charline Minerbe",
+        username:"twilight",
+        email:"charline@gmail.com"
+    },
+    {
+        id:3,
+        name:"laurent Minerbe",
+        username:"laulaunidas",
+        email:"laulaunidas@gmail.com"
+    },
+    
+];
